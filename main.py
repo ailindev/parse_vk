46aaa78e2027b1603ca2de49b5142610209e1dc8
ailindev <ailin.webdeveloper@gmail.com>
2019-07-02 04:11:55 +0500

    delete unnecessary loop

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ from bs4 import BeautifulSoup as bs
 
 # Пишем то, как будут запрашиваться данные
 headers = {
-    'accept': '*/*',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36',
-    'cookie': 'remixmdevice=1920/1080/1/!!-!!!!;'
+   'accept': '*/*',
+   'User-Agent': 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36',
+   'cookie': 'remixmdevice=1920/1080/1/!!-!!!!;'
 }
 
 # Ссылка, с которой будут взяты данные
@@ -27,21 +27,19 @@ base_url = input('Введите ссылку на видео: \n')
 
 # Изменение ссылки на мобильную версию сайта
 if re.match('https://vk.com', base_url):
-    base_url = base_url.replace('https://', 'https://m.')
+   base_url = base_url.replace('https://', 'https://m.')
 
 
 def vk_parse(base_url, headers):
-    session = requests.Session()
-    request = session.get(base_url, headers=headers)
-    if request.status_code == 200:
-        soup = bs(request.content, 'html.parser')
-        div = soup.find_all('div', attrs={'class': 'vv_body'})
-        for source in div:
-            src = source.find('source', attrs={'type': 'video/mp4'})['src']
-            print("\nТеперь видео доступно для скачивания: \n" + src)
-
-    else:
-        print('ERROR')
+   session = requests.Session()
+   request = session.get(base_url, headers=headers)
+   if request.status_code == 200:
+      soup = bs(request.content, 'html.parser')
+      div = soup.find('div', attrs={'class': 'vv_body'})
+      src = div.find('source', attrs={'type': 'video/mp4'})['src']
+      print("\nТеперь видео доступно для скачивания: \n" + src)
+   else:
+      print('ERROR')
 
 
 vk_parse(base_url, headers)
